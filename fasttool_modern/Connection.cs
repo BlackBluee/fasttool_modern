cfcbc9ecea9e786a4d6066ffcac4641457ff61a7
BlackBluee <kacperpe38@gmail.com>
2024-10-10 22:22:45 +0200

    add connection class

---
+++ fasttool_modern/Connection.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fasttool_modern
+{
+
+    public class Connection
+    {
+        private Connection connection;
+        private SerialPort serialPort;
+        public event Action<string> DataReceived;
+        string portName = "COM7"; // Ustaw odpowiedni port COM
+        int baudRate = 115200;
+
+        private static Connection instance;
+
+        private Connection() { }
+
+        public static Connection Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Connection();
+                }
+                return instance;
+            }
+        }
+
+
+        public void connectDevice()
+        {
+            serialPort = new SerialPort(portName, baudRate);
+
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            try
+            {
+                if (!serialPort.IsOpen)
+                {
+                    serialPort.Open(); // Otwórz port
+                    getInfoDevice();
+                }
+                else
+                {
+                    //MessageBox.Show("Port jest już otwarty.");
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                //MessageBox.Show("Błąd: Port jest już używany przez inną aplikację.");
+            }
+            catch (IOException ioEx)
+            {
+                //MessageBox.Show("Błąd: Problem z wejściem/wyjściem - " + ioEx.Message);
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Błąd: " + ex.Message);
+            }
+
+
+        }
+
+        public bool askConnection()
+        {
+            if (serialPort.IsOpen && serialPort != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void comSend(string message)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.WriteLine(message);
+            }
+        }
+
+        public void getInfoDevice()
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.WriteLine("InfoDevice");
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string response = serialPort.ReadLine();
+            // Wywołanie zdarzenia, gdy dane zostaną odebrane
+            DataReceived?.Invoke(response);
+        }
+
+        public void connect()
+        {
+            serialPort.Open();
+        }
+        public void disconnect()
+        {
+            serialPort.Close();
+        }
+
+        public string getPortName()
+        {
+            return serialPort.PortName;
+        }
+    }
+}
--- fasttool_modern/MainWindow.xaml.cs
+++ fasttool_modern/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -9,9 +9,17 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
+using System.Timers;
+using System.Runtime.InteropServices;
 using Windows.Foundation.Collections;
+using System.Threading;
+using AudioSwitcher.AudioApi.CoreAudio;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,12 +31,61 @@ namespace fasttool_modern
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        private string previousProcessName = "";
+        //private ContextMenuStrip trayMenu; //menu kontekstowe
+
+        // Inicjalizacja kontrolera audio
+
+
+        private const int APPCOMMAND_MEDIA_NEXTTRACK = 11;
+        private const int APPCOMMAND_MEDIA_PREVIOUSTRACK = 12;
+        private const int APPCOMMAND_MEDIA_PLAY_PAUSE = 14;
+        private const int WM_APPCOMMAND = 0x0319;
+        private CancellationTokenSource cancellationTokenSource;
+        private CoreAudioController audioController;
+        private CoreAudioDevice defaultPlaybackDevice;
+        private System.Timers.Timer deviceCheckTimer;
+        private System.Timers.Timer windowCheckTimer;
+
+        Connection connection = Connection.Instance;
         public MainWindow()
         {
             this.InitializeComponent();
             NavView.SelectedItem = NavView.MenuItems[0];
+            audioController = new CoreAudioController();
+            defaultPlaybackDevice = audioController.DefaultPlaybackDevice;
         }
-        
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            connection.connectDevice();
+            StartConnectionChecker();
+            // Inicjalizacja timera - jeśli potrzebujesz timera w środowisku niewizualnym, wielowątkowym lub w aplikacji konsolowej.
+            deviceCheckTimer = new System.Timers.Timer();
+            deviceCheckTimer.Interval = 1000; // Sprawdzanie co sekundę
+            deviceCheckTimer.Elapsed += OnDeviceCheckTimerElapsed;
+            deviceCheckTimer.Start();
+            // Inicjalizacja timera - jeśli pracujesz w aplikacji Windows Forms i potrzebujesz okresowo aktualizować UI.
+            //windowCheckTimer.Tick += new EventHandler(CheckActiveWindow);
+            //windowCheckTimer.Start();
+
+        }
+
         private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
             var selectedItem = args.SelectedItem as NavigationViewItem;
@@ -61,5 +118,100 @@ namespace fasttool_modern
         {
             myButton.Content = "Clicked";
         }*/
+
+        private void CheckActiveWindow(object sender, EventArgs e)
+        {
+            // Pobranie uchwytu aktywnego okna
+            IntPtr handle = GetForegroundWindow();
+
+            // Pobranie ID procesu związanego z aktywnym oknem
+            GetWindowThreadProcessId(handle, out uint processId);
+
+            try
+            {
+                // Pobranie nazwy procesu
+                Process process = Process.GetProcessById((int)processId);
+                string currentProcessName = process.ProcessName;
+
+                // Aktualizacja etykiety, jeśli nazwa procesu się zmieniła
+                if (currentProcessName != previousProcessName)
+                {
+                    
+
+                    previousProcessName = currentProcessName;
+                    
+
+                    //Type:AcitveApp,Name: chrome
+                    connection.comSend("Type:ActiveApp,Name:" + currentProcessName);
+
+                }
+            }
+            catch (ArgumentException)
+            {
+                // Proces mógł zostać zakończony, zresetuj nazwę procesu
+                previousProcessName = "";
+                
+            }
+        }
+
+        //Sprawdzanie połączenia z urządzeniem
+        private void StartConnectionChecker()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            Task.Run(() =>
+            {
+                while (!cancellationTokenSource.Token.IsCancellationRequested)
+                {
+                    if (connection.askConnection())
+                    {
+                        //statusLabel.Text = "Device: connected";
+                        
+
+                        //btnIconButton.Image = Properties.Resources.nazwa_ikony;
+                    }
+                    else
+                    {
+                        //statusLabel.Text = "Device: disconnected";
+                        
+                    }
+                    Console.WriteLine("Sprawdzanie w tle...");
+                    Thread.Sleep(1000); // Opóźnienie 1 sekunda
+                }
+            }, cancellationTokenSource.Token);
+        }
+
+        private void StopConnectionChecker()
+        {
+            cancellationTokenSource?.Cancel();
+        }
+
+        private static void SendMediaKey(int key)
+        {
+            IntPtr hWnd = GetForegroundWindow();
+            SendMessageW(hWnd, WM_APPCOMMAND, hWnd, (IntPtr)(key << 16));
+        }
+
+        //do sprawdzenia zmiany urządzenia audio
+        private void OnDeviceCheckTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            var currentDefaultDevice = audioController.DefaultPlaybackDevice;
+
+            if (currentDefaultDevice.Id != defaultPlaybackDevice.Id)
+            {
+                defaultPlaybackDevice = currentDefaultDevice;
+                Console.WriteLine("Domyślne urządzenie odtwarzania zmienione.");
+            }
+        }
+        private void RunExternalApplication(string path)
+        {
+            try
+            {
+                Process.Start(path);
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show($"Błąd uruchamiania aplikacji: {ex.Message}");
+            }
+        }
     }
 }
