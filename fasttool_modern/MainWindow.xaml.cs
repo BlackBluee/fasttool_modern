5f08565887d872f1bf702a336fd9d43e9ccbdbc3
BlackBluee <kacperpe38@gmail.com>
2024-10-10 18:12:16 +0200

    Dodaj pliki projektów.

---
+++ fasttool_modern/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace fasttool_modern
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            NavView.SelectedItem = NavView.MenuItems[0];
+        }
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            var selectedItem = args.SelectedItem as NavigationViewItem;
+            var pageTag = selectedItem.Tag.ToString();
+
+            switch (pageTag)
+            {
+                case "DashboardPage":
+                    ContentFrame.Navigate(typeof(DashboardPage));
+                    NavView.Header = "Dashboard";
+                    break;
+
+                case "ProfilesPage":
+                    ContentFrame.Navigate(typeof(ProfilesPage));
+                    NavView.Header = "Profiles";
+                    break;
+
+                case "ConnectionPage":
+                    ContentFrame.Navigate(typeof(ConnectionPage));
+                    NavView.Header = "Connection";
+                    break;
+
+                case "SettingsPage":
+                    ContentFrame.Navigate(typeof(SettingsPage));
+                    NavView.Header = "Settings";
+                    break;
+            }
+        }
+        /*private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }*/
+    }
+}
